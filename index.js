1060d18fb07d0610561e7d02791d5f7121dc12f7
alanshalem <alanshalem@gmail.com>
2022-03-17 08:53:43 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import conectarDB from './config/db.js';
+import usuarioRoutes from './routes/usuarioRoutes.js';
+const app = express();
+
+dotenv.config();
+
+conectarDB();
+
+// Routing
+app.use('/api/usuarios', usuarioRoutes);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+	console.log('Server is running on port 4000');
+});
---
+++ routes/usuarioRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	res.send('DESDE API/USUARIOS');
+});
+
+router.post('/', (req, res) => {
+	res.send('POST API/USUARIOS');
+});
+
+export default router;
